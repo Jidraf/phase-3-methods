3bcb50951ece2802be2d1fa5603023640d2e8c73
Jidraf Kimachia <raphjid170@gmail.com>
2022-08-18 14:08:50 +0300

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer 
+
+def greet name
+    puts "Hello, #{name}!"  
+end
+
+def greet_with_default(default="Naureen")
+    puts "Hello, #{default}!"
+    puts "Hello, programmer!"
+end
+greet_with_default 
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+puts add(1,2)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    number / 2
+end
